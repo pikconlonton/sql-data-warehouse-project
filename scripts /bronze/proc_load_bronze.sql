5515d680e45f947e24999511dd3fa0a500ae5fad
QuanPM <138176489+ioqyeworiu@users.noreply.github.com>
2025-05-05 20:02:58 +0700

    Update proc_load_bronze.sql

--- scripts /bronze/proc_load_bronze.sql
+++ scripts /bronze/proc_load_bronze.sql
@@ -19,7 +19,6 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
---EXEC  bronze.load_bronze
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
@@ -30,15 +29,20 @@ BEGIN
 		--bronze.Patients
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.Patients';
-				TRUNCATE TABLE  bronze.Patients;
+		TRUNCATE TABLE  bronze.Patients;
 		PRINT '>> Inserting Data Into:  bronze.Patients';
-		BULK INSERT bronze.Patients 
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\patients.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Patients 
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\patients.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Patients (
+			patient_id, name, gender, dob, address, phone, email
+		)
+		SELECT id, address, dob, email, gender, name, phone
+		FROM hms_hqt.dbo.patient
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -46,15 +50,20 @@ BEGIN
 		--bronze.Doctors
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.Doctors';
-				TRUNCATE TABLE  bronze.Doctors;
+		TRUNCATE TABLE  bronze.Doctors;
 		PRINT '>> Inserting Data Into:  bronze.Doctors';
-		BULK INSERT bronze.Doctors
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\doctors.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Doctors
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\doctors.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Doctors (
+			doctor_id, name, last_name, specialization, phone, email
+		)
+		SELECT id, name, last_name, specialization, phone, email
+		FROM hms_hqt.dbo.doctor AS DO;
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -62,15 +71,20 @@ BEGIN
 		--bronze.Appointments
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.Appointments';
-				TRUNCATE TABLE  bronze.Appointments;
+		TRUNCATE TABLE  bronze.Appointments;
 		PRINT '>> Inserting Data Into:  bronze.Appointments';
-		BULK INSERT bronze.Appointments
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\appointments.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Appointments
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\appointments.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Appointments (
+			appointment_id, patient_id, doctor_id, appointment_date, start_time, end_time, reason, status, phone, location, appointment_type
+		)
+		SELECT id, patient_id, doctor_id, appointment_date, start_time, end_time, reason, status, phone, location, appointment_type
+		FROM hms_hqt.dbo.appointment
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -80,13 +94,18 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.VitalSigns';
 				TRUNCATE TABLE  bronze.VitalSigns;
 		PRINT '>> Inserting Data Into:  bronze.VitalSigns';
-		BULK INSERT bronze.VitalSigns
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\vital_signs.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.VitalSigns
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\vital_signs.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.VitalSigns (
+			vital_id, patient_id, measurement_date, blood_pressure, heart_rate, respiratory_rate, temperature, oxygen_saturation, blood_sugar
+		)
+		SELECT id, patient_id, measurement_date, blood_pressure, heart_rate, respiratory_rate, temperature, oxygen_saturation, blood_sugar
+		FROM hms_hqt.dbo.vital_sign
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -96,13 +115,18 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.Diseases';
 				TRUNCATE TABLE  bronze.Diseases;
 		PRINT '>> Inserting Data Into:  bronze.Diseases';
-		BULK INSERT bronze.Diseases
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\diseases.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Diseases
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\diseases.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Diseases (
+			disease_id, patient_id, disease_name, diagnosis_date
+		)
+		SELECT id, patient_id, disease_name, diagnosis_date
+		FROM hms_hqt.dbo.disease
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -110,15 +134,20 @@ BEGIN
 		--bronze.Treatments
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.Treatments';
-				TRUNCATE TABLE  bronze.Treatments;
+		TRUNCATE TABLE  bronze.Treatments;
 		PRINT '>> Inserting Data Into:  bronze.Treatments';
-		BULK INSERT bronze.Treatments
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\treatments.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Treatments
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\treatments.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Treatments(
+			treatment_id, patient_id, doctor_id, disease_id, treatment_description, treatment_date
+		)
+		SELECT id, patient_id, doctor_id, disease_id, treatment_description, treatment_date
+		FROM hms_hqt.dbo.treatment
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -128,13 +157,18 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.HospitalFees';
 				TRUNCATE TABLE  bronze.HospitalFees;
 		PRINT '>> Inserting Data Into:  bronze.HospitalFees';
-		BULK INSERT bronze.HospitalFees 
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\hospital_fee.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.HospitalFees 
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\hospital_fee.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.HospitalFees (
+			fee_id, appointment_id, patient_id, service_type, description, amount, fee_date, phone
+		)
+		SELECT id, appointment_id, patient_id, service_type, description, amount, fee_date, phone
+		FROM hms_hqt.dbo.hospital_fee
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -144,13 +178,18 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.Prescriptions';
 				TRUNCATE TABLE  bronze.Prescriptions;
 		PRINT '>> Inserting Data Into:  bronze.Prescriptions';
-		BULK INSERT bronze.Prescriptions 
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\prescriptions.csv'
-		WITH (
-			FORMAT = 'CSV',
-			CODEPAGE = '65001',
-			FIRSTROW = 2
-		);
+		--BULK INSERT bronze.Prescriptions 
+		--FROM  'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\prescriptions.csv'
+		--WITH (
+		--	FORMAT = 'CSV',
+		--	CODEPAGE = '65001',
+		--	FIRSTROW = 2
+		--);
+		INSERT INTO bronze.Prescriptions (
+			prescription_id, appointment_id, doctor_id, patient_id, medicine_name, form, dosage_mg, instruction, duration_days, note
+		)
+		SELECT id, appointment_id, doctor_id, patient_id, medicine_name, form, dosage_mg, instruction, duration_days, note
+		FROM hms_hqt.dbo.prescription
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
@@ -158,15 +197,20 @@ BEGIN
 		--bronze.Lab_results
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table: bronze.Lab_results';
-				TRUNCATE TABLE bronze.Lab_results;
-		PRINT '>> Inserting Data Into:  bronze.Lab_results';
-		BULK INSERT bronze.Lab_results
-		FROM  'D:\BTL HQTCSDL\data1k\data1k\process_csv\lab_results.csv'
+		TRUNCATE TABLE bronze.Labresults;
+		PRINT '>> Inserting Data Into: bronze.Lab_results';
+		BULK INSERT bronze.LabResults
+		FROM 'E:\UNIVERSITY SUBJECTS\PTIT subjects\Database management system\BTL\dataset\lab_results.csv'
 		WITH (
 			FORMAT = 'CSV',
 			CODEPAGE = '65001',
 			FIRSTROW = 2
 		);
+		--INSERT INTO bronze.LabResults (
+		
+		--)
+		--SELECT 
+		--FROM hms_hqt.dbo
 		SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT ' -----------------------------------------------';
